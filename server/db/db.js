ef14c60c9fba1bdc5ec78709e732805b6b90b5dc
Savannaha <savwiley@yahoo.com>
2021-09-30 15:34:06 -0500

    update to connect with PostgreSQL

--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,10 @@
 const Sequelize = require("sequelize");
 
-const db = new Sequelize(process.env.DATABASE_URL || "postgres://localhost:5432/messenger", {
-  logging: false
+const db = new Sequelize('messenger', 'savannaha', 'hatchways', {
+  host: 'localhost',
+  port: '5432',
+  dialect: 'postgres',
+  logging: false,
 });
 
 module.exports = db;
