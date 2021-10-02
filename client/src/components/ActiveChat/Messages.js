f4562a95b72e0bca8823c23bd9cfad7ec89051f2
Savannaha <savwiley@yahoo.com>
2021-10-02 16:48:20 -0500

    reverse order of messages array so older posts appear on top in the UI

--- client/src/components/ActiveChat/Messages.js
+++ client/src/components/ActiveChat/Messages.js
@@ -8,7 +8,7 @@ const Messages = (props) => {
 
   return (
     <Box>
-      {messages.map((message) => {
+      {messages.slice(0).reverse().map((message) => {
         const time = moment(message.createdAt).format("h:mm");
 
         return message.senderId === userId ? (
