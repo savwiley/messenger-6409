d292431108d85b9801310ff96861e0406a405519
Savannaha <savwiley@yahoo.com>
2021-10-05 21:40:46 -0500

    add base style to sign up & login pages

--- client/src/Login.js
+++ client/src/Login.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Redirect, useHistory } from "react-router-dom";
 import { connect } from "react-redux";
+import backgroundImage from "./images/bg-img.png";
+import bubbleIcon from "./images/bubble.svg";
 import {
   Grid,
   Box,
@@ -8,10 +10,70 @@ import {
   Button,
   FormControl,
   TextField,
+  makeStyles,
 } from "@material-ui/core";
 import { login } from "./store/utils/thunkCreators";
 
+const useStyles = makeStyles(() => ({
+  root: {
+    fontFamily: "'Open Sans', sans-serif",
+    width: "40%",
+    padding: "2% 10%",
+    alignSelf: "center",
+  },
+  sideImage: {
+    display: "inline-flex",
+    flexDirection: "column",
+    height: "100vh",
+    width: "40%",
+    background: `linear-gradient(180deg, rgba(58, 141, 255, 0.85), rgba(134, 185, 255, 0.85)), center/cover no-repeat url(${backgroundImage})`,
+    color: '#FFF',
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+  },
+  top: {
+    width: "fit-content",
+    alignItems: "center",
+    position: "absolute",
+    top: "2%",
+    right: "2%",
+  },
+  topText: {
+    color: "#BBB",
+    fontSize: 12,
+    paddingRight: 20,
+    cursor: "default",
+  },
+  topButton: {
+    boxShadow: "0 0 8px rgba(0,0,0,0.1)",
+    color: "#3A8DFF",
+    fontFamily: "'Montserrat', sans-serif",
+    width: 200,
+    height: 50,
+  },
+  header: {
+    fontSize: 40,
+    fontWeight: 600,
+    cursor: "default",
+  },
+  bottomButton: {
+    display: "block",
+    background: "#3A8DFF",
+    color: "#FFF",
+    boxShadow: "none",
+    fontFamily: "'Montserrat', sans-serif",
+    margin: "20px auto 0",
+    width: 200,
+    height: 50,
+    '&:hover': {
+      background: "#6abaff",
+    }
+  },
+}));
+
 const Login = (props) => {
+  const classes = useStyles();
   const history = useHistory();
   const { user, login } = props;
 
@@ -28,16 +90,21 @@ const Login = (props) => {
   }
 
   return (
-    <Grid container justify="center">
-      <Box>
-        <Grid container item>
-          <Typography>Need to register?</Typography>
-          <Button onClick={() => history.push("/register")}>Register</Button>
+    <Grid container>
+      <Grid className={classes.sideImage}>
+        <img src={bubbleIcon} alt="Chat Bubble" style={{ width: '15%', marginBottom: 40 }} />
+        <Typography style={{ fontSize: 30 }}>Converse with anyone<br />with any language</Typography>
+      </Grid>
+      <Box className={classes.root}>
+        <Grid container item className={classes.top}>
+          <Typography className={classes.topText}>Don't have an account?</Typography>
+          <Button onClick={() => history.push("/register")} className={classes.topButton}>Create account</Button>
         </Grid>
         <form onSubmit={handleLogin}>
           <Grid>
+            <Typography className={classes.header}>Welcome back!</Typography>
             <Grid>
-              <FormControl margin="normal" required>
+              <FormControl margin="normal" style={{ width: "100%" }} required>
                 <TextField
                   aria-label="username"
                   label="Username"
@@ -46,16 +113,16 @@ const Login = (props) => {
                 />
               </FormControl>
             </Grid>
-            <FormControl margin="normal" required>
+            <FormControl margin="normal" style={{ width: "100%" }} required>
               <TextField
-                label="password"
+                label="Password"
                 aria-label="password"
                 type="password"
                 name="password"
               />
             </FormControl>
             <Grid>
-              <Button type="submit" variant="contained" size="large">
+              <Button type="submit" variant="contained" size="large" className={classes.bottomButton}>
                 Login
               </Button>
             </Grid>
--- client/src/Signup.js
+++ client/src/Signup.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { Redirect, useHistory } from "react-router-dom";
 import { connect } from "react-redux";
+import backgroundImage from "./images/bg-img.png";
+import bubbleIcon from "./images/bubble.svg";
 import {
   Grid,
   Box,
@@ -9,10 +11,70 @@ import {
   FormControl,
   TextField,
   FormHelperText,
+  makeStyles,
 } from "@material-ui/core";
 import { register } from "./store/utils/thunkCreators";
 
+const useStyles = makeStyles(() => ({
+  root: {
+    fontFamily: "'Open Sans', sans-serif",
+    width: "40%",
+    padding: "2% 10%",
+    alignSelf: "center",
+  },
+  sideImage: {
+    display: "inline-flex",
+    flexDirection: "column",
+    height: "100vh",
+    width: "40%",
+    background: `linear-gradient(180deg, rgba(58, 141, 255, 0.85), rgba(134, 185, 255, 0.85)), center/cover no-repeat url(${backgroundImage})`,
+    color: '#FFF',
+    alignItems: "center",
+    justifyContent: "center",
+    textAlign: "center",
+  },
+  top: {
+    width: "fit-content",
+    alignItems: "center",
+    position: "absolute",
+    top: "2%",
+    right: "2%",
+  },
+  topText: {
+    color: "#BBB",
+    fontSize: 12,
+    paddingRight: 20,
+    cursor: "default",
+  },
+  topButton: {
+    boxShadow: "0 0 8px rgba(0,0,0,0.1)",
+    color: "#3A8DFF",
+    fontFamily: "'Montserrat', sans-serif",
+    width: 200,
+    height: 50,
+  },
+  header: {
+    fontSize: 40,
+    fontWeight: 600,
+    cursor: "default",
+  },
+  bottomButton: {
+    display: "block",
+    background: "#3A8DFF",
+    color: "#FFF",
+    boxShadow: "none",
+    fontFamily: "'Montserrat', sans-serif",
+    margin: "20px auto 0",
+    width: 200,
+    height: 50,
+    '&:hover': {
+      background: "#6abaff",
+    }
+  },
+}));
+
 const Login = (props) => {
+  const classes = useStyles();
   const history = useHistory();
   const { user, register } = props;
   const [formErrorMessage, setFormErrorMessage] = useState({});
@@ -37,45 +99,47 @@ const Login = (props) => {
   }
 
   return (
-    <Grid container justify="center">
-      <Box>
-        <Grid container item>
-          <Typography>Need to log in?</Typography>
-          <Button onClick={() => history.push("/login")}>Login</Button>
+    <Grid container>
+      <Grid className={classes.sideImage}>
+        <img src={bubbleIcon} alt="Chat Bubble" style={{ width: '15%', marginBottom: 40 }} />
+        <Typography style={{ fontSize: 30 }}>Converse with anyone<br />with any language</Typography>
+      </Grid>
+      <Box className={classes.root}>
+        <Grid container item className={classes.top}>
+          <Typography className={classes.topText}>Already have an account?</Typography>
+          <Button onClick={() => history.push("/login")} className={classes.topButton}>Login</Button>
         </Grid>
         <form onSubmit={handleRegister}>
           <Grid>
+            <Typography className={classes.header}>Create an account.</Typography>
             <Grid>
-              <FormControl>
+              <FormControl margin="normal" style={{ width: "100%" }} required>
                 <TextField
                   aria-label="username"
                   label="Username"
                   name="username"
                   type="text"
-                  required
                 />
               </FormControl>
             </Grid>
             <Grid>
-              <FormControl>
+              <FormControl margin="normal" style={{ width: "100%" }} required>
                 <TextField
                   label="E-mail address"
                   aria-label="e-mail address"
                   type="email"
                   name="email"
-                  required
                 />
               </FormControl>
             </Grid>
             <Grid>
-              <FormControl error={!!formErrorMessage.confirmPassword}>
+              <FormControl error={!!formErrorMessage.confirmPassword} margin="normal" style={{ width: "100%" }} required>
                 <TextField
                   aria-label="password"
                   label="Password"
                   type="password"
                   inputProps={{ minLength: 6 }}
                   name="password"
-                  required
                 />
                 <FormHelperText>
                   {formErrorMessage.confirmPassword}
@@ -83,21 +147,20 @@ const Login = (props) => {
               </FormControl>
             </Grid>
             <Grid>
-              <FormControl error={!!formErrorMessage.confirmPassword}>
+              <FormControl error={!!formErrorMessage.confirmPassword} margin="normal" style={{ width: "100%" }} required>
                 <TextField
                   label="Confirm Password"
                   aria-label="confirm password"
                   type="password"
                   inputProps={{ minLength: 6 }}
                   name="confirmPassword"
-                  required
                 />
                 <FormHelperText>
                   {formErrorMessage.confirmPassword}
                 </FormHelperText>
               </FormControl>
             </Grid>
-            <Button type="submit" variant="contained" size="large">
+            <Button type="submit" variant="contained" size="large" className={classes.bottomButton}>
               Create
             </Button>
           </Grid>
