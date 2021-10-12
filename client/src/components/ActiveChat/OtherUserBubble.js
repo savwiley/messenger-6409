1539cc06e8516383da823806a32634d91962a4d1
Savannaha <savwiley@yahoo.com>
2021-10-12 13:12:22 -0500

    remove duplicated code

--- client/src/components/ActiveChat/OtherUserBubble.js
+++ client/src/components/ActiveChat/OtherUserBubble.js
@@ -1,52 +1,19 @@
 import React, { useEffect, useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import { Box, Typography, Avatar } from "@material-ui/core";
+import { ChatBubbles } from "./bubbles";
 
-const useStyles = makeStyles(() => ({
-  root: {
-    display: "flex"
-  },
-  avatar: {
-    height: 30,
-    width: 30,
-    marginRight: 11,
-    marginTop: 6
-  },
-  usernameDate: {
-    fontSize: 11,
-    color: "#BECCE2",
-    fontWeight: "bold",
-    marginBottom: 5
-  },
-  bubble: {
-    backgroundImage: "linear-gradient(225deg, #6CC1FF 0%, #3A8DFF 100%)",
-    borderRadius: "0 10px 10px 10px",
-    overflow: "hidden",
-    margin: "0 10px 10px 0",
-    '& img': {
-      maxWidth: "250px",
-      maxHeight: "200px",
-      marginBottom: "-5px",
-    }
-  },
-  imageBox: {
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center"
-  },
-  text: {
-    fontSize: 14,
-    fontWeight: "bold",
-    color: "#FFFFFF",
-    letterSpacing: -0.2,
-    padding: 8,
-  }
-}));
+const styling = {
+  color: "#FFFFFF", 
+  background: "linear-gradient(225deg, #6CC1FF 0%, #3A8DFF 100%)", 
+  border: "0 10px 10px 10px", 
+  margin: "0 10px 10px 0", 
+  align: "left"
+};
 
 const OtherUserBubble = (props) => {
-  const classes = useStyles();
   const [multiples, setMultiples] = useState(false);
   const { text, time, otherUser, image } = props;
+
+  const info = { text, time, otherUser, image };
 
   useEffect(() => {
     if (image) {
@@ -55,41 +22,7 @@ const OtherUserBubble = (props) => {
   }, [image]);
 
   return (
-    <Box>
-      {multiples ? (
-        <Box className={classes.root}>
-          <Avatar alt={otherUser.username} src={otherUser.photoUrl} className={classes.avatar}></Avatar>
-          {text && 
-            <Box className={classes.bubble}>
-              <Typography className={classes.text}>{text}</Typography>
-            </Box>
-          }
-          <Box className={classes.imageBox}>
-            {image.map(e => {
-              return <Box className={classes.bubble}><img src={e} alt="send success" /></Box>
-            })}
-          </Box>
-            <Typography className={classes.usernameDate}>
-              {otherUser.username} {time}
-            </Typography>
-        </Box>
-      ) : (
-        <Box className={classes.root}>
-          <Avatar alt={otherUser.username} src={otherUser.photoUrl} className={classes.avatar}></Avatar>
-          <Box>
-            <Typography className={classes.usernameDate}>
-              {otherUser.username} {time}
-            </Typography>
-            <Box className={classes.bubble}>
-              {image && <img src={image} alt="send success" />}
-              {text && <Typography className={classes.text}>{text}</Typography>}
-            </Box>
-          </Box>
-        </Box>
-        )
-      }
-        
-    </Box>
+    <ChatBubbles multiple={multiples} other={true} style={styling} information={info} />
   );
 };
 
--- client/src/components/ActiveChat/SenderBubble.js
+++ client/src/components/ActiveChat/SenderBubble.js
@@ -1,50 +1,19 @@
 import React, { useEffect, useState } from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import { Box, Typography } from "@material-ui/core";
+import { ChatBubbles } from "./bubbles";
 
-const useStyles = makeStyles(() => ({
-  root: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "flex-end",
-    marginBottom: "10px",
-  },
-  date: {
-    fontSize: 11,
-    color: "#BECCE2",
-    fontWeight: "bold",
-    marginBottom: 5
-  },
-  text: {
-    fontSize: 14,
-    color: "#91A3C0",
-    letterSpacing: -0.2,
-    padding: 8,
-    fontWeight: "bold",
-    textAlign: "right"
-  },
-  imageBox: {
-    display: "flex",
-    flexDirection: "row",
-    alignItems: "center"
-  },
-  bubble: {
-    background: "#F4F6FA",
-    borderRadius: "10px 10px 0 10px",
-    overflow: "hidden",
-    margin: "0 0 10px 10px",
-    '& img': {
-      maxWidth: "250px",
-      maxHeight: "200px",
-      marginBottom: "-5px",
-    }
-  }
-}));
+const styling = {
+  color: "#91A3C0", 
+  background: "#F4F6FA", 
+  border: "10px 10px 0 10px", 
+  margin: "0 0 10px 10px", 
+  align: "right"
+};
 
 const SenderBubble = (props) => {
-  const classes = useStyles();
   const [multiples, setMultiples] = useState(false);
   const { time, text, image } = props;
+
+  const info = { text, time, image };
 
   useEffect(() => {
     if (image) {
@@ -53,32 +22,8 @@ const SenderBubble = (props) => {
   }, [image]);
 
   return (
-    <Box>
-      {multiples ? (
-        <Box className={classes.root}>
-          {text && 
-            <Box className={classes.bubble}>
-              <Typography className={classes.text}>{text}</Typography>
-            </Box>
-          }
-          <Box className={classes.imageBox}>
-            {image.map(e => {
-              return <Box className={classes.bubble}><img src={e} alt="send success" /></Box>
-            })}
-          </Box>
-          <Typography className={classes.date}>{time}</Typography>
-        </Box> 
-        ) : (
-        <Box className={classes.root}>
-          <Typography className={classes.date}>{time}</Typography>
-          <Box className={classes.bubble}>
-            {image && image.map(e => {return <img src={e} alt="send success" />})}
-            {text && <Typography className={classes.text}>{text}</Typography>}
-          </Box>
-        </Box>
-        )
-      }
-    </Box>
+    
+    <ChatBubbles multiple={multiples} other={false} style={styling} information={info} />
   );
 };
 
